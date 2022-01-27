450b4d2831920a575049a37d219469a92298ac5d
Santiago Valencia <santiago.valencia@itconsultores.com.co>
2022-01-27 16:16:45 -0500

    :tada: first code in go Say the Numbers challenge soloLearn

---
+++ Sololearn/go/SayTheNumbers.go
@@ -0,0 +1,56 @@
+// Say The Numbers
+
+
+// You are making a robot that can speak numbers.
+// Your robot should take 3 numbers in the range of 0-10 as input and output the corresponding texts in English.
+
+// Sample Input:
+// 8
+// 0
+// 5
+
+// Sample Output:
+// Eight
+// Zero
+// Five
+
+package main
+import "fmt"
+
+func switchFunc(o int){
+    switch o {
+        case 1:
+        fmt.Println("One")
+        case 2:
+        fmt.Println("Two")
+        case 3:
+        fmt.Println("Three")
+        case 4:
+        fmt.Println("Four")
+        case 5:
+        fmt.Println("Five")
+        case 6:
+        fmt.Println("Six")
+        case 7:
+        fmt.Println("Seven")
+        case 8:
+        fmt.Println("Eight")
+        case 9:
+        fmt.Println("Nine")
+        case 10:
+        fmt.Println("Ten")
+        default:
+        fmt.Println("Zero")
+    }
+}
+
+func main() {
+    //your code goes here
+    var i,m,n int
+    fmt.Scanln(&i)
+    fmt.Scanln(&m)
+    fmt.Scanln(&n)
+    switchFunc(i)
+    switchFunc(m)
+    switchFunc(n)
+}
