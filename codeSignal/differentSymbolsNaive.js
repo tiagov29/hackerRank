410807e78277d6be13eae9c29c53befbd7a4121f
Santiago Valencia <santiago.valencia@itconsultores.com.co>
2022-03-12 06:48:05 -0500

    working in different symbols naive code signal challenge

---
+++ codeSignal/differentSymbolsNaive.js
@@ -0,0 +1,18 @@
+// Given a string, find the number of different characters in it.
+
+// Example
+
+// For s = "cabca", the output should be
+// solution(s) = 3.
+
+// There are 3 different characters a, b and c.
+function solution(s) {
+  let letters = s.split("");
+  let numberOfletters = [];
+  letters.map((el) => {
+    if (!numberOfletters.includes(el)) {
+      numberOfletters.push(el);
+    }
+  });
+  return numberOfletters.length;
+}
